11988f2b5e8f1759897bd16bac8e02e42378805a
mrinaldhillon <mrinaldhillon@gmail.com>
2017-12-09 09:55:00 -0500

    * Hw5 Polygon class unit testcases using googletest and matchers using *	googlemock

---
+++ hw5/lib/triangle.cc
@@ -0,0 +1,31 @@
+#include "lib/triangle.h"
+#include <math.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include "lib/point.h"
+// Should <memory> be included here since it has already been included in header
+
+using namespace std;
+
+namespace polygon {
+Triangle::Triangle(Point a, Point b, Point c) {
+  a_ = a;
+  b_ = b;
+  c_ = c;
+}
+
+const std::vector<Point> Triangle::GetPoints() const {
+  return vector<Point>{a_, b_, c_};
+}
+
+double Triangle::GetArea() const {
+  double ab = a_.Distance(b_);
+  double bc = b_.Distance(c_);
+  double ac = a_.Distance(c_);
+  double s = (ab + bc + ac) / 2.0;
+  return sqrt(s * (s - ab) * (s - bc) * (s - ac));
+}
+
+const std::string& Triangle::GetType() const { return type_; }
+}
