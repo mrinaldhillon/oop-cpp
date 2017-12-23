b219b895d5b62b87be8a88a3686026c5e4450026
mrinaldhillon <mrinaldhillon@gmail.com>
2017-12-23 14:29:51 -0500

    * Final checking for submission * TODO: Code cleanup and refactoring

--- build.sh
+++ build.sh
@@ -8,57 +8,59 @@ HELP_TEXT="HELP ....
 bash build.sh			# builds all
 bash build.sh prompt		# all with continue prompt
 bash build.sh clean		# clean all
-bash build.sh hwN		# build  Nth homework"
+bash build.sh hwN		# build Nth homework"
+
+function build_one ()
+{
+		echo "Starting Build for $@ *****************************"
+		cd "${SCRIPT_DIR}/$@" && bash build.sh
+		echo "Finshed Build for $@ *****************************"
+}
 
 function build_all ()
 {
 	find * -prune -type d | grep hw | while IFS= read -r d; do
-		echo "Starting Build for ${d} *****************************  "
-		cd "${SCRIPT_DIR}/${d}" && bash build.sh
-		echo "Finshed Build for ${d} ***************************** "
+		build_one "${d}"
 	done
 }
 
 function build_all_prompt ()
 {
 	find * -prune -type d | grep hw | while IFS= read -r d; do
-		echo "Starting Build for ${d} *****************************"
-		cd "${SCRIPT_DIR}/${d}" && bash build.sh
-		echo "Finshed Build for ${d} *****************************"
+		build_one "${d}"
 		echo "Press key to continue";
-		read -p "Conitnue? [yn]" answer </dev/tty
+		read -p "Conitnue? [y/n]" answer </dev/tty
 		[ $answer = "n" ] && return 0
 	done
 }
 
+function clean_one ()
+{
+		echo "Starting cleanup for $@ *****************************"
+		cd "${SCRIPT_DIR}/$@" && bash build.sh clean
+		echo "Finshed cleanup for $@ *****************************"
+}
+
 function clean_all ()
 {
+	[ ! -z "$@" ] && clean_one "$@" && return $?
 	find * -prune -type d | grep hw | while IFS= read -r d; do
-		echo "Starting cleanup for ${d} *****************************"
-		cd "${SCRIPT_DIR}/${d}" && bash build.sh clean
-		echo "Finshed cleanup for ${d} *****************************"
+		clean_one "${d}"
 	done
 }
 
-function build_hw ()
-{
-	echo "Starting Build for $@ ***************************** "
-	cd $@ && bash build.sh
-	echo "Finished Build for $@ ***************************** "
-}
-
-
 if [ ! -z "$1" ]; then
 	if [ $1 = "clean" ]; then
-		clean_all
+		clean_all "${2}"
 	elif [ -d "${1}" ]; then
-		build_hw "${1}"
+		build_one "${1}"
 	elif [ "${1}" = "prompt" ]; then
 		build_all_prompt
 	else
 
 		echo "${HELP_TEXT}" && exit 1
 	fi
+	exit $?
 else 
 	build_all
 fi
--- hw14/build.sh
+++ hw14/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat ./README
+[ "$#" -ne 1 ] && cat ./README
